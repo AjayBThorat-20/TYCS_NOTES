83a3841a8deeba49dfcc974555131a4d95a4132f
krunal dhavle <krunaldhavle1999@gmail.com>
2020-09-30 13:45:42 +0530

    ws prac2

---
+++ ws prac/prac2/prac2b/WcfService2/WcfService2/Service1.svc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading;
+
+namespace WcfService2
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string RequestReplyPattern()
+        {
+            //using the namespace using System.Threading for sleep the thread in 2 minute  
+            Thread.Sleep(2000);
+            //return the date and time  
+            return "current time of Service is" + DateTime.Now.ToString();
+        }
+    }
+}
---
+++ ws prac/prac2/prac2b/WcfService2/WcfService2/WebForm1.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WcfService2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            // Before service is call the time will show  
+            lblPagedate.Text = "Page date is " + DateTime.Now.ToString();
+            ServiceReference1.Service1Client sc = new ServiceReference1.Service1Client();
+            //service is calling and in label, time is show after service will call  
+            lblServicedate.Text = sc.RequestReplyPattern();
+        }
+    }
+}
